a8af84fd3ea0dccb0fa6a5f91543495b14ec2df9
Chandan Gowda <chandansuresh007@gmail.com>
2022-09-18 14:14:32 +0530

    sendinblue api key updated

--- lib/utils/secret.dart
+++ lib/utils/secret.dart
@@ -1,7 +1,7 @@
 
 //NOTE: These tokens are for testing purposes only. Since this is a private repo, we have not added it to gitignore.
 
-final String sendInBlueApiKey = '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';
+final String sendInBlueApiKey = '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';
 final String razorpayKeyId = 'rzp_test_8dntPDjVrkC3Sj';
 final String razorpayKeySecret = 'HYydouxmjOgclXGxkzkRQp2h';
 final String firebaseApiUrl = 'https://billmi-default-rtdb.firebaseio.com';
