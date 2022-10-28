11bbddfd8fa6a020299f74cb4ae5abfdeb5f8d2d
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-10-28 16:16:40 +0530

    updated

--- lib/utils/secret.dart
+++ lib/utils/secret.dart
@@ -1,5 +1,5 @@
 
-//NOTE: These tokens are for testing purposes only. Since this is a private repo, we have not added it to gitignore.
+//NOTE: These tokens are for testing purposes only. 
 
 final String sendInBlueApiKey = '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';
 final String razorpayKeyId = 'rzp_test_8dntPDjVrkC3Sj';
